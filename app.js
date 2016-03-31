9abf4562c32ee27050b98765ba6e6e2ca985cbc9
error25 <autovincelynch@gmail.com>
2016-03-31 08:24:56 +0100

    still need to turn the array of base64 into images

--- app.js
+++ app.js
@@ -19,7 +19,32 @@ app.get('/', function(req, res) {
 io.on('connect', function(socket) {
   console.log("User connected with socket id of: " + socket.conn.id);
   socket.on('message', function(message) {
-    console.log(message);
+    console.log(message.lastimage);
+
+
+    //create knox AWS client.
+    var AWSclient = knox.createClient({
+      key: process.env.AWS_ACCESS_KEY,
+      secret: process.env.AWS_SECRET_KEY,
+      bucket: config.AWS.bucket I HAVENT MADE THIS YET
+    });
+
+        var img = message.lastimage[0]; // just get the first (for the moment).
+        var data = img.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(data, 'base64');
+
+        var re = AWSclient.put(message.username._id+'.png', {
+          'Content-Length': buf.length,
+          'Content-Type': 'img/png'
+        });
+        re.on('response', function(resp){
+          if(200 == resp.statusCode) {
+            console.log("Uploaded to" + re.url);
+            pl.coverImg = re.url; //prepare to save image url in the database
+            pl.save(function(error){
+              if(error) console.log(error);
+              else{
+
     io.emit('message', message);
   });
 });
@@ -31,6 +56,10 @@ YOU NEED TO GET THE ARRAY of BASE64 GIF files, and SAVE THOSE as GIFS on AMAZON 
 
 Then you need to send each of those to the Google Cloud Vision Processor. 
 
+
+////////////// Changed my mind
+
+http://stackoverflow.com/questions/23620470/node-js-saving-canvas-as-an-image-for-heroku-hosted-apps-using-amazon-s3
 app.use('/scripts/gifshot', express.static(__dirname + '/node_modules/gifshot/build/'));
 
 server.listen(port, function() {
