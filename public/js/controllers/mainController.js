514837facf3668445ddbafdc8269cf9ed16b92b9
error25 <autovincelynch@gmail.com>
2016-04-05 11:24:27 +0100

    did some styling, really want more instagrams, I think regardless of how I present the information, whether I include charts or allow search, having some kind of visual element is important

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,8 +1,8 @@
 angular.module('VinceLynch')
 .controller('MainController', MainController);
 
-MainController.$inject = ['$auth', 'tokenService', '$resource', '$window', '$state'];
-function MainController($auth, tokenService, $resource, $window, $state) {
+MainController.$inject = ['$auth', 'tokenService', '$resource', '$window', '$state', 'GEOCODER_API_KEY', '$http', 'GUARDIAN_API_KEY'];
+function MainController($auth, tokenService, $resource, $window, $state, GEOCODER_API_KEY, $http, GUARDIAN_API_KEY) {
   var self = this;
 
   self.loginDeets = {};
@@ -16,8 +16,40 @@ function MainController($auth, tokenService, $resource, $window, $state) {
   self.summary = {};
   self.detailed = {};
   self.wikicity = {};
-
+  self.country = {};
+  self.guardianarticles = [];
   
+ // http://content.guardianapis.com/search?q=millennials%2C%20cities&api-key=
+
+var Guardian = $resource('http://content.guardianapis.com/search?q=millennials%2C%20cities&api-key=' + GUARDIAN_API_KEY, {}, {
+});
+
+this.getGuardianArticles= function(){
+  Guardian.get(function(data){
+    console.log('success, got Guardian Articles: ', data);  
+   self.guardianarticles = data.response.results;
+   console.log(self.guardianarticles);
+    }, function(err){
+    console.log('Guardian Articles API - request failed');
+    });
+}
+this.getGuardianArticles();
+
+
+var Geocoder = $resource('https://maps.googleapis.com/maps/api/geocode/json?address=' + self.selected.city + "+" + self.selected.country +  '&key=' + GEOCODER_API_KEY, {}, {
+});
+
+
+this.getGeoCode = function(){
+  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + self.selected.city + "+" + self.selected.country +  '&key=' + GEOCODER_API_KEY).success(function(data){
+    console.log(data.results[0].geometry.location);
+    self.selected.lat = data.results[0].geometry.location.lat;
+    self.selected.lng = data.results[0].geometry.location.lng;
+    self.InstaLocationGet(self.selected.lat,self.selected.lng)
+  });
+}
+
+
 var Wikicity = $resource('http://localhost:8000/api/wikicity/:city', {}, {
   get: { cache: true, method: 'get' }
 });
@@ -30,6 +62,12 @@ this.getWikicityData = function(city){
       console.log('Wikicity Internal API - request failed');
       });
   }
+
+this.getLatLngRESTcountries = function(city){
+  $http.get('https://restcountries.eu/rest/v1/capital/' + city).success(function(data){
+    console.log(data);
+  });
+}
 
 var Numtable = $resource('http://localhost:8000/api/numtable/:city/:country', {}, {
   get: { cache: true, method: 'get' }
@@ -45,15 +83,16 @@ this.getCityNumTableData = function(city, country){
     });
 }
 
-
 this.gotoCity = function(country){  //lodash
-
-  var city = $window._.find(Cities, { name: country }).capital;
+  self.country = $window._.find(Cities, { name: country });
+  var city = self.country.capital;
+  console.log(self.country)
   console.log("city is: " + city);
 
   self.selected = {city: city, country: country};
   self.getCityNumTableData(city,country)
   self.getWikicityData(city)
+  self.getGeoCode();
 
   $state.transitionTo('city', {city: city, country: country});
 }
@@ -68,16 +107,18 @@ this.gotoCity = function(country){  //lodash
 
   
 
-this.getWikiTable = function(){
-  Wikitable.get({wikipage: "List_of_countries_by_inequality-adjusted_HDI"},function(data){
+this.getWikiTable = function(wikititle){
+  Wikitable.get({wikipage: wikititle},function(data){
     data.rows.shift();
     console.log('success, got wikitable: ', data.rows);  
-    self.wikitable = data.rows;   
+    self.wikitable = data.rows; 
+    var lat = data.rows.wikisummary  
+    // self.InstaLocationGet(lat, lng) 
     }, function(err){
     console.log('WikiTable Internal API - request failed');
     });
   }
-  self.getWikiTable();
+  
   
 
 
@@ -85,10 +126,33 @@ this.getWikiTable = function(){
     get: { cache: true, method: 'get' }
   });
 
+  this.createGifFromInstas = function(){
+    gifshot.createGIF({
+      'images': self.instagrams,
+      'text': self.selected.city,
+      'gifWidth': 400,
+      'gifHeight': 300,
+      'fontSize': '30px',
+      'interval': 0.25,
+      'resizeFont': true,
+      'textBaseline': 'bottom',
+       //['http://i.imgur.com/2OO33vX.jpg', 'http://i.imgur.com/qOwVaSN.png', 'http://i.imgur.com/Vo5mFZJ.gif']
+    },function(obj) {
+      if(!obj.error) {
+        var image = obj.image,
+        animatedImage = document.createElement('img');
+        animatedImage.src = image;
+        document.body.appendChild(animatedImage);
+      }
+    });
+  }
+
   this.InstaLocationGet = function(lat, lng) {
     self.instagrams = Instagram.get({lat: lat, lng: lng}, function(data){
     console.log('success, got instagrams for this location: ', data);     
     self.instagrams = data.images.images;  
+    self.instagrams.pop();
+    self.createGifFromInstas() // create Gifs for City
     }, function(err){
     console.log('API Instagram Location - request failed');
     });
